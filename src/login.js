a11fb9e123517455bebf4043ed59f22c9aa75ccd
dwayne valdelievre <dwayne.valdelievre@gmail.com>
2018-02-21 13:04:36 +0000

    refactored the login page

--- src/login.js
+++ src/login.js
@@ -1,24 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Route, NavLink, HashRouter} from "react-router-dom";
+import { Route, NavLink, HashRouter, Link } from "react-router-dom";
 import MainResearcher from './MainResearcher';
 
 class Login extends Component {
 	constructor(props){
 		super(props);
 		this.handleClick = this.handleClick.bind(this);
+		this.state = {
+			showComponent: true
+		}
 	}
 	
 	handleClick(){
-		var butt = document.getElementById("button_id");
-		butt.div.setState({ showResults: false });
+		//var foobar = document.getElementById("button_id");
+		//foobar.div.setState({ showResults: false });
+		//this.setState{showResults: false};
+		//this.props.isHidden ? null : <Login />;
+		//this.setState({showComponent: !this.state.showComponent})
+		//this.state.showComponent && <Login />
 	}
 
 render() {
   return (
 			<HashRouter>
 				<div className="Login">
-				<button id = "button_id" onClick={this.handleClick}><NavLink exact to="/MainResearcher">Login</NavLink></button>
+				<button id = "button_id" onClick={this.handleClick}>
+					<NavLink exact to="/MainResearcher">Login</NavLink>
+				</button>		
 				<Route path="/MainResearcher" component={MainResearcher}/>
 				</div>
 			</HashRouter>	
