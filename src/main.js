4c7b0f1bb1e28053b7c13505112d3f08b46590cd
JAStrachan <jstrachansss@gmail.com>
2018-02-22 11:22:12 +0000

    Creating links to the project page

--- src/main.js
+++ src/main.js
@@ -2,14 +2,16 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import ProjectsListResearcher from './projectsListResearcher'
 import CreateProject from './createProject'
+import ProjectPage from './projectPage'
 
 
 const Main = () => (
   <main>
     <Switch>
-	  
+
       <Route exact path='/' component={ProjectsListResearcher}/>
       <Route path='/createProject' component={CreateProject}/>
+      <Route path='/projectPage' component={ProjectPage}/>
     </Switch>
   </main>
 )
